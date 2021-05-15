ead9a8d4341b872cce6d2b7cd3363b40e267393d
imjord <dean.jay.goat@gmail.com>
2021-05-15 14:14:59 -0600

    short js practice

---
+++ script.js
@@ -0,0 +1,28 @@
+// variable = assigned to my age
+let myAge = 20;
+
+// making var assiened value of 2 kept as let will be changed
+let earlyYears = 2;
+
+
+// 2 is getting multiplied by 10.5 and then the new value is the output between 2 and 10.5
+
+earlyYears *= 10.5;
+
+// creating var later years and assining its value of my age - 2;
+let laterYears = myAge - 2;
+
+// using muliplication assignment operator again.
+laterYears *= 4;
+
+
+console.log(earlyYears, laterYears);
+
+myAgeInDogYears = earlyYears + laterYears;
+
+
+var myName = 'Jordan'.toLowerCase();
+
+
+
+alert(`My name is ${myName} I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years`);
